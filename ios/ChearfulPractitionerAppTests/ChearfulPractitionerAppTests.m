b89dd3060d4e36d9213df2db529ff0a4cb486abc
Muaaz bin sarfraz <muaazkhan1575@gmail.com>
2023-07-17 13:38:26 +0500

    App name Changed

--- ios/ChearfulPractitionerAppTests/ChearfulPractitionerAppTests.m
+++ ios/ChearfulPractitionerAppTests/ChearfulPractitionerAppTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface chearfulAppTests : XCTestCase
+@interface ChearfulPractitionerAppTests : XCTestCase
 
 @end
 
-@implementation chearfulAppTests
+@implementation ChearfulPractitionerAppTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
