c4f3d38ca447092f3aec44c9d9e3131db89821d4
Muaaz bin sarfraz <muaazkhan1575@gmail.com>
2023-09-11 16:03:50 +0500

    changes to prod

--- src/api/endpoints.js
+++ src/api/endpoints.js
@@ -2,7 +2,7 @@
 const Prod_Base_URL="https://chearful.com/api/v2"
 const Dev_Base_URL="https://dev.chearful.com/api/v2"
 
-export const BASE_URL =Dev_Base_URL
+export const BASE_URL =Prod_Base_URL
 
 
 // GET REQUESTS
