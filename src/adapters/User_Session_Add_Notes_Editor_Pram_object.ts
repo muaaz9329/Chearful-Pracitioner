74ef2d09c5c3bf9dcea64dc84aa7e78beb0d30cf
Abdullah Sajjad <contact@abdullahsajjad.me>
2023-09-12 17:13:44 +0500

    fix(adapter not found): fixed an issue of an adapter not found
    updated adapter name and resolved the issue of adapter not found during
    the build causing failure

--- src/adapters/User_Session_Add_Notes_Editor_Pram_object.ts
+++ src/adapters/User_Session_Add_Notes_Editor_Pram_object.ts
@@ -1,19 +1,32 @@
-import { User_Session_notes_pram_object } from "./User_Session_notes_pram_object";
+import { UserSessionNotesParamObj } from "./User_Session_Notes_Pram_object";
 
-export class User_Session_Add_Notes_Editor_Pram_object{
-    mode:string;
-    ClientData:User_Session_notes_pram_object;
-    TypeOfNote:"text"|"canvas"|"img"|"pdf"|'docx';
-    ComingFor:'update'|'upload';
-    content:string;
-    routeLoc:"back"|"Prac_Session" | "Prac_ClientDetail" |"Prac_AddNoteSession"
-    constructor(MODE:string , Data:User_Session_notes_pram_object ,TypeOfNote:"text"|"canvas"|"img"|"pdf"|'docx',Action:'update'|'upload' , routeLoc:"back"|"Prac_Session" | "Prac_ClientDetail" |"Prac_AddNoteSession" ){
-        this.mode = MODE;
-        this.ClientData = Data;
-        this.TypeOfNote = TypeOfNote;
-        this.ComingFor = Action;
-        this.content='';
-        this.routeLoc = routeLoc;
-
-    }
+export class User_Session_Add_Notes_Editor_Pram_object {
+  mode: string;
+  ClientData: UserSessionNotesParamObj;
+  TypeOfNote: "text" | "canvas" | "img" | "pdf" | "docx";
+  ComingFor: "update" | "upload";
+  content: string;
+  routeLoc:
+    | "back"
+    | "Prac_Session"
+    | "Prac_ClientDetail"
+    | "Prac_AddNoteSession";
+  constructor(
+    MODE: string,
+    Data: UserSessionNotesParamObj,
+    TypeOfNote: "text" | "canvas" | "img" | "pdf" | "docx",
+    Action: "update" | "upload",
+    routeLoc:
+      | "back"
+      | "Prac_Session"
+      | "Prac_ClientDetail"
+      | "Prac_AddNoteSession"
+  ) {
+    this.mode = MODE;
+    this.ClientData = Data;
+    this.TypeOfNote = TypeOfNote;
+    this.ComingFor = Action;
+    this.content = "";
+    this.routeLoc = routeLoc;
+  }
 }
